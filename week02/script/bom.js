00d3abebad97f37410c5b82c236a04dbc14a8143
Sogiefa-12 <sogiefa2@gmail.com>
2024-06-22 15:27:19 +0100

    commit

---
+++ week02/script/bom.js
@@ -0,0 +1,38 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
+let li =document.createElement('li');
+let deleteButton = document.createElement('button');
+button.addEventListener('click', function() {
+    console.log('Handler called');
+    if (input.value.length) {
+        // Handle button click
+        let li =document.createElement('li');
+
+        let deleteButton = document.createElement('button');
+
+        li.textContent = input.value;
+        deleteButton.textContent = 'X';
+
+        li.append(deleteButton);
+
+        list.append(li);
+    } else {
+        alert("please enter a chapter title");
+        input.focus();
+        return;
+    }
+});
+
+
+
+deleteButton.addEventListener('click', () => {
+    if (input.value.length){
+        deleteButton.textContent = 'X';
+        li.append(deleteButton);
+        list.removeChild(li);
+        input.focus();
+        input.value = '';
+    };
+   
+});
