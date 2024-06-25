f5a011665a38d24f93c188cdcb22f98afa1f4aca
Sogiefa-12 <sogiefa2@gmail.com>
2024-06-25 14:47:45 +0100

    commit

---
+++ scripts/getmenu.js
@@ -0,0 +1,8 @@
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	hamButton.classList.toggle('open');
+	navigation.classList.toggle('open');
+});
+
