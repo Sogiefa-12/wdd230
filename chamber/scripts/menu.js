0981da5f8d5ffaea9f651550b1dd00294b4c2d4e
Sogiefa-12 <sogiefa2@gmail.com>
2024-07-03 15:40:21 +0100

    comm

--- chamber/scripts/menu.js
+++ chamber/scripts/menu.js
@@ -6,3 +6,16 @@ hamButton.addEventListener('click', () => {
 	navigation.classList.toggle('open');
 });
 
+
+// Get the current date
+const date = new Date();
+
+// Format the date in the desired format
+const formattedDate = date.toLocaleDateString('en-US', {
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric'
+});
+
+// Update the DOM with the last modified date
+document.getElementById('date').innerHTML = formattedDate;
