0c530d7d10630d773a8da2d83e78ec64e187e255
Sogiefa-12 <sogiefa2@gmail.com>
2024-07-20 17:57:35 +0100

    commit

--- scripts/weather.js
+++ scripts/weather.js
@@ -1,39 +1,35 @@
+const city = 'Abuja,Nigeria';
+const apiKey = 'c4420ce86b47a02637420f83b4358983';
+const weatherEndpoint = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
 
-
-const currentTemp = document.querySelector('#current-temp');
-const weatherIcon = document.querySelector('#weather-icon');
-const captionDesc = document.querySelector('figcaption');
-
-let city ='Abuja,Nigeria';
-let apiKey = 'c4420ce86b47a02637420f83b4358983';
-
-
-
-
-const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
-
-async function apiFetch() {
-try {
-    const response = await fetch(url);
-    if (response.ok) {
-        const data = await response.json();
-        console.table(data);
-        return data;
+fetch(weatherEndpoint)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
     }
-    else {
-        throw Error(await response.text());
+    return response.json();
+  })
+  .then(data => {
+    const { main, weather, list } = data;
+    const currentTemp = main.temp;
+    const fahrenheitTemp = (currentTemp * (9/5)) + 32;
+    const currentWeather = weather[0].description;
+
+    document.querySelector('#current-temp').innerText = `Current temperature: ${fahrenheitTemp}°F`;
+    document.querySelector('#current-weather').innerText = `Current weather: ${currentWeather}`;
+
+    if (list) {
+      const threeDayForecast = list.slice(0, 3);
+
+      threeDayForecast.forEach((forecast) => {
+        const li = document.createElement('li');
+        const fahrenheitTemp = (forecast.main.temp * (9/5)) + 32;
+        li.innerText = `Forecast: ${forecast.dt_txt} - ${fahrenheitTemp}°F`;
+        document.querySelector('#three-day-forecast').appendChild(li);
+      });
     }
-}catch (error) {
-    console.log(error);
-}
-}
 
-apiFetch();
-
-function displayResults() {
-apiFetch().then((data) => {
-    currentTemp.innerHTML = `${data.main.temp}&deg;F`;
-    weatherIcon.src = 'https://openweathermap.org/img/wn/' + data.weather[0].icon + '@2x.png';
-    captionDesc.innerHTML = data.weather[0].description;
-});
-}
+    document.querySelector('#weather-icon').src = `http://openweathermap.org/img/w/${weather[0].icon}.png`;
+    document.querySelector('figcaption').innerText = `${weather[0].description}`;
+  })
+  .catch(error => console.error('Error:', error));
